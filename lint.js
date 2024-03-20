de88c42622577cdb7bc1c35fb59519f51ea56f63
James Sharpe <james.sharpe@zenotech.com>
2024-03-20 17:39:52 +0000

    Update lint.js

--- lint.js
+++ lint.js
@@ -7,21 +7,25 @@ function lint(filename, disabled = []) {
     let fail_outputs = false;
     let fail_execution_count = false;
 
-    for (let i = 0; i < json.cells.length; ++i) {
+    if (has_key(json, 'cells')) {
+        for (let i = 0; i < json.cells.length; ++i) {
 
-        const cell = json.cells[i];
+            const cell = json.cells[i];
 
-        if (!fail_outputs && !disabled.includes('outputs') && has_key(cell, 'outputs')) {
-            if (Array.from(cell['outputs']).length > 0) {
-                fail_outputs = true;
+            if (!fail_outputs && !disabled.includes('outputs') && has_key(cell, 'outputs')) {
+                if (Array.from(cell['outputs']).length > 0) {
+                    fail_outputs = true;
+                }
+            }
+
+            if (!fail_execution_count && !disabled.includes('execution_count') && has_key(cell, 'execution_count')) {
+                if (cell['execution_count'] != null) {
+                    fail_execution_count = true;
+                }
             }
         }
-
-        if (!fail_execution_count && !disabled.includes('execution_count') && has_key(cell, 'execution_count')) {
-            if (cell['execution_count'] != null) {
-                fail_execution_count = true;
-            }
-        }
+    } else {
+        console.log(`${filename}: has no cells`);
     }
 
     // Warn users about which failures are present in this file
